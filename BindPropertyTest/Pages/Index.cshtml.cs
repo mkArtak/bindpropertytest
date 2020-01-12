fd8b81a9d748e0f6cfe108467a3c8249045e5d22
Sanjeev Manickam <smanickam@gmail.com>
2020-01-12 11:29:57 -0500

    intial commit

---
+++ BindPropertyTest/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace BindPropertyTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public bool Test1 { get; set; }
+
+        [BindProperty]
+        public bool Test2 { get; set; }
+
+        public void OnGet()
+        {
+            Test1 = false;
+            Test2 = false;
+        }
+
+        public void OnPost()
+        {
+            Console.WriteLine("Test1: {0}", Test1);
+            Console.WriteLine("Test2: {0}", Test2);
+
+            Test1 = !Test1;
+            Test2 = !Test2;
+        }
+    }
+}
